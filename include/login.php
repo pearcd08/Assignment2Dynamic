71eedc234c85674032853f488d63d20c7a9defa5
pearcd08 <pearcd08@myunitec.ac.nz>
2020-12-08 19:35:58 -0800

    update

--- include/login.php
+++ include/login.php
@@ -51,7 +51,7 @@ if (!isset($_SESSION["userID"])){
 <form action="<?php $_SERVER["PHP_SELF"]; ?>"   method="post">
     <input name="username" type ="text" placeholder="Username">
     <input name="password" type ="password" placeholder="Password">
-    <input name="submit" value="Post">
+    <input type="submit" value="Post">
 
 </form>
 <?php
