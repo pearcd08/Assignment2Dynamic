c9af21d5fcf058f7f88f5c1be2bb379fd02d636d
pearcd08 <pearcd08@myunitec.ac.nz>
2020-12-10 06:28:37 -0800

    update as

--- include/index.php
+++ include/index.php
@@ -22,8 +22,9 @@
 
 
     <div id = "menu">
-        <?php include "menu.php"; ?>
         <?php include "login.php"; ?>
+        <?php include "menu.php"; ?>
+
     </div>
 <div id="main">
     <h1>Products of the Week</h1>
